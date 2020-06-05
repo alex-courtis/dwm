28d3599ae9d7c593c5a76f45e8caf110e6b79077
Alexander Courtis <alex@courtis.org>
2020-06-05 14:37:20 +1000

    remove unused dwm default colours

--- config.h
+++ config.h
@@ -9,11 +9,6 @@ static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=11" };
 static const int fontpt             = 11;       /* used for calculating borderpx */
 static const char dmenufont[]       = "monospace:size=11";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
 
 /* base16-bright */
 static const char base00[] = "#000000"; // Default Background
